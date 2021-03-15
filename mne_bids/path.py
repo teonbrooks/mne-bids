b551b351266846639293eea46c34f0b657b8afc0
Adam Li <adam2392@gmail.com>
2021-03-14 23:05:55 -0400

    Update mne_bids/path.py
    Co-authored-by: Richard Höchenberger <richard.hoechenberger@gmail.com>

--- mne_bids/path.py
+++ mne_bids/path.py
@@ -286,7 +286,7 @@ class BIDSPath:
     root : str | pathlib.Path
         The root for the filename to be created. E.g., a path to the folder
         in which you wish to create a file with this name.
-        Defaults to ``'.'``.
+        Defaults to ``'.'``, i.e. the current working directory.
     check : bool
         If ``True``, enforces BIDS conformity. Defaults to ``True``.
 
